917959e036fa598804a18e31c15151edc1187dca
Uchenna <okorocode@gmail.com>
2019-05-29 21:58:50 +0100

    basic logic started

---
+++ app.js
@@ -0,0 +1,11 @@
+
+(function() {
+    console.log('working')
+
+    const form = document.querySelector('#task-form');
+    const tastInput = document.querySelector('#task-input')
+
+    form.addEventListener('submit', function() {
+
+    })
+}())
