aa730ddf54bfb2b318a4a14758952c9746fd980b
Uchenna <okorocode@gmail.com>
2019-05-30 16:37:43 +0100

    simple logic done!

--- app.js
+++ app.js
@@ -3,9 +3,20 @@
     console.log('working')
 
     const form = document.querySelector('#task-form');
-    const tastInput = document.querySelector('#task-input')
+    const taskInput = document.querySelector('#task-input');
+    const taskContainer = document.querySelector('#task-container')
 
-    form.addEventListener('submit', function() {
+    form.addEventListener('submit', function(e) {
+        
+        e.preventDefault()
+
+        const task = taskInput.value
+
+        const htmlContent = `<div><input type="checkbox">${task}</div>`
+
+        taskContainer.insertAdjacentHTML('afterbegin', htmlContent);
+
+        taskInput.value = ''
 
     })
 }())
