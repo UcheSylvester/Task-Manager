ee761eec727f7a6b286e83b6d67f067d35d3568e
Uchenna <okorocode@gmail.com>
2019-06-05 21:34:15 +0100

    basic todo done, but without LS

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
     const form = document.querySelector('#task-form');
     const taskInput = document.querySelector('#task-input');
-    const taskContainer = document.querySelector('#task-container');
+    const taskContainer = document.querySelector('#todo-container');
     let tasks = []
 
     form.addEventListener('submit', function(e) {
@@ -14,11 +14,7 @@
         const task = taskInput.value;
         const htmlContent = `
             <div class="task">
-        let checkboxes = document.querySelectorAll('input[type="checkbox"]');
-        checkboxes.forEach(checkbox => checkbox.addEventListener('click', done));
-        function done() {
-            (this.checked) ? this.nextElementSibling.classList.add('done') : this.nextElementSibling.classList.remove('done');         
-        }                <input class="checkbox" name="tasks" type="checkbox">
+                <input class="checkbox" name="tasks" type="checkbox">
                 <label for="${task}">${task}</label>
             </div>
         `
@@ -27,17 +23,12 @@
 
         // marking each task when done
 
+        const checkboxes = document.querySelectorAll('input[type]');
 
-        tasks.push(task);
-        // console.log(tasks);
+        checkboxes.forEach(checkbox => checkbox.addEventListener('click', (e) => e.target.checked ? e.target.nextElementSibling.classList.add('done') : e.target.nextElementSibling.classList.remove('done')))
+        
 
-        function save() {
-            const taskString = JSON.stringify(tasks)
-            console.log(taskString)
-            window.localStorage.setItem('tasks', taskString)
-        }
-
-        save()
+        // console.log(tasks)
         /*
         The above can be done with the one-liner below
         NOTE: with arrow function as an event callback, we loss access to  `this`
--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-'use strict';
-
-console.log('working')
-
+'use strict'
 
 /** 
 console.log('working');
