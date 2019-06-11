7e172a1cbdc2480c0be1407b15489174f6a9b55e
Uchenna <okorocode@gmail.com>
2019-06-11 21:43:00 +0100

    defined starter constant variables and minor naming adjustment

---
+++ scripts.js
@@ -0,0 +1,5 @@
+console.log('working...');
+
+const form = document.querySelector('#task-form');
+const taskInput = doucment.querySelector('#task-input');
+const taskContainer = document.querySelector('#task-container')
