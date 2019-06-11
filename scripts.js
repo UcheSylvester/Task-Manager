b124ccff813c43eb504459b51d354af0f71da459
Uchenna <okorocode@gmail.com>
2019-06-11 22:42:50 +0100

    stored task in tasks array

--- scripts.js
+++ scripts.js
@@ -1,5 +1,16 @@
 console.log('working...');
 
 const form = document.querySelector('#task-form');
-const taskInput = doucment.querySelector('#task-input');
-const taskContainer = document.querySelector('#task-container')
+const taskInput = document.querySelector('#task-input');
+const taskContainer = document.querySelector('#task-container');
+let tasks = [];
+
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const task = taskInput.value;
+    tasks.push(task)
+    taskInput.value = '';
+    console.log(tasks)
+})
