17bcabd1c36fd9a80a86d1197762d08390801e78
Uchenna <okorocode@gmail.com>
2019-06-15 00:13:56 +0100

    tasks displayed on the page

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ const form = document.querySelector('#task-form');
 const taskInput = document.querySelector('#task-input');
 const taskContainer = document.querySelector('#task-container');
 let tasks = [];
+let htmlContent = ''
 
 
 form.addEventListener('submit', function (e) {
@@ -13,4 +14,24 @@ form.addEventListener('submit', function (e) {
     tasks.push(task)
     taskInput.value = '';
     console.log(tasks)
-})
+
+    displayTasks(tasks)
+});
+
+function displayTasks(tasks) {
+    htmlContent = tasks.map(task => `
+        <div class="task">
+            <input type="checkbox">${task} <br>
+        </div>
+    `).join('')
+
+
+    if (taskContainer.hasChildNodes()) {
+        taskContainer.innerHTML = '';
+        taskContainer.insertAdjacentHTML('afterbegin', htmlContent);
+    } else {
+        taskContainer.insertAdjacentHTML('afterbegin', htmlContent);
+
+    }
+
+}
