0d25606a48408cf3ffbcf262496f91fc44b1222a
Uchenna <okorocode@gmail.com>
2019-06-26 03:01:23 +0100

    save and display tasks from localstorage

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 console.log('working');
 
-const taskContainer = document.querySelector('.task-container');
+const taskContainer = document.getElementById('task-container');
+const form = document.getElementById('task-form')
 
 // fetching tasks from localstorage
 
-function fetchTask() {
+function fetchTasks() {
     const tasks = JSON.parse(localStorage.getItem('tasks'));
 
     taskContainer.innerHTML = ''
@@ -12,10 +13,45 @@ function fetchTask() {
     // looping through tasks to display
     tasks.forEach(task => {
         let description = task.description,
-            status = 'done';
+            status = task.status;
 
         taskContainer.innerHTML += `<div class="task">
             <input type="checkbox"> ${description} <br>
         <div>`
     })
 }
+
+function saveTask(e) {
+    e.preventDefault()
+
+    const description = document.getElementById('task-input').value;
+    console.log(description);
+
+    const task = {
+        description: description,
+        status: 'undone'
+    }
+
+    // saving to localStorage
+    if(window.localStorage) {
+        // when localStorage is empty 
+        if(localStorage.getItem('tasks') === null) {
+            let tasks = [];
+            tasks.push(task);
+            localStorage.setItem('tasks', JSON.stringify(tasks))
+        } else {
+            let tasks = JSON.parse(localStorage.getItem('tasks'));  // set tasks to tasks in localstorage
+            tasks.push(task);
+            localStorage.setItem('tasks', JSON.stringify(tasks))
+        }
+    }
+
+    fetchTasks()
+
+    form.reset();
+
+
+
+}
+
+form.addEventListener('submit', saveTask);
